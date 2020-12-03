52a8d86ef00b30a565e56f8fa50ad2568ee62f42
Siddhant Priyadarshi <siddhantp4ever@gmail.com>
2020-12-04 01:32:57 +0530

    meme_generator

---
+++ src/components/App.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import './App.css';
+import Meme from '../abis/Meme.json'
+
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
+
+class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const networkId = await web3.eth.net.getId()
+    const networkData = Meme.networks[networkId]
+    if(networkData) {
+      const abi = Meme.abi
+      const address = networkData.address
+      const contract = web3.eth.Contract(abi, address)
+      this.setState({ contract })
+      const memeHash = await contract.methods.get().call()
+      this.setState({ memeHash })
+    } else {
+      window.alert('Smart contract not deployed to detected network.')
+    }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      buffer: null,
+      memeHash: '',
+      contract: null
+    }
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  captureFile = (event) => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) })
+    }
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    console.log("Submitting file to ipfs...")
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('Ipfs result', result)
+      const memeHash = result[0].hash
+      this.setState({ memeHash })
+      if(error) {
+        console.error(error)
+        return
+      }
+       this.state.contract.methods.set(result[0].hash).send({ from: this.state.account }).then((r) => {
+         return this.setState({ memeHash })
+       })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="http://www.dappuniversity.com/bootcamp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Daily Memes
+          </a>
+          <ul className="navbar-nav" px-3>
+            <li className="nav-item text-nowrap d=none d-sm-none d-sm-block">
+              <small className="text-white">{this.state.account}</small>
+            </li>  
+          </ul>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={`https://ipfs.infura.io/ipfs/${this.state.memeHash}`} alt="Meme Of The Day"/>
+                </a>
+                <p>&nbsp;</p>
+                <h2>Change Meme</h2>
+                <form onSubmit={this.onSubmit} >
+                  <input type='file' onChange={this.captureFile} />
+                  <input type='submit' />
+                </form>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
